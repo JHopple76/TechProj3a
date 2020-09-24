493642e50e8b28b7f036f0177401385854a2d99b
JHopple76 <70179411+JHopple76@users.noreply.github.com>
2020-09-24 15:45:23 -0400

    Update Program.cs

--- TechProj3a/Program.cs
+++ TechProj3a/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Author: T. Jarrett Hopple
+// Class: ISM 4300
+// Assignment: Tech Project 3a
+using System;
 
 namespace TechProj3a
 {
@@ -6,7 +9,20 @@ namespace TechProj3a
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // when opened, program shows "Please enter your name"
+            Console.WriteLine("Please enter your name. ");
+
+            // allows user to type name in 
+            string name;
+            name = Console.ReadLine();
+            // after typing in name, message appears as "Hello (Name typed)
+            Console.WriteLine("\r\nHello " + name + " - Go Bulls");
+
+            // ends and exits the program
+            Console.WriteLine("\r\nPress any key to exit");
+            Console.ReadKey(true);
+        } // static end
+    } // class end
+} // namespace end
+
+// This code never shows up when a publish it to github from VS
